7fa20aa8f39b15e53d80115588e6ceff7832103b
baptiste <projetsbaptiste@laposte.net>
2023-08-11 08:34:35 +0200

    rename the project with snake_case

---
+++ aoc_2015/target/debug/aoc-2015.d
@@ -0,0 +1,1 @@
+/home/baptiste/Documents/rust/projects/AOC-2015/aoc_2015/target/debug/aoc-2015: /home/baptiste/Documents/rust/projects/AOC-2015/aoc_2015/src/main.rs
---
+++ aoc_2015/target/debug/deps/aoc_2015-282e8e6671bedf48.d
@@ -0,0 +1,8 @@
+/home/baptiste/Documents/rust/projects/AOC-2015/aoc_2015/target/debug/deps/aoc_2015-282e8e6671bedf48.rmeta: src/main.rs Cargo.toml
+
+/home/baptiste/Documents/rust/projects/AOC-2015/aoc_2015/target/debug/deps/aoc_2015-282e8e6671bedf48.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
